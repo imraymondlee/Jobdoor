8fd70be1b4152a2cff4a9d1999691fdc17ed4bde
imraymondlee <imleeraymond@gmail.com>
2019-12-13 13:36:33 -0800

    Style pagination buttons and change favicon

--- src/app/components/postings/postings.component.ts
+++ src/app/components/postings/postings.component.ts
@@ -12,6 +12,7 @@ export class PostingsComponent implements OnInit {
   totalPages:number;
   totalPagesArr:number[];
   currentPage:number;
+  event = event;
 
   constructor(private postingService: PostingService) { }
 
@@ -26,11 +27,11 @@ export class PostingsComponent implements OnInit {
         this.postings = posts.data;
         this.totalPages = posts.totalPages;
         this.totalPagesArr = Array(this.totalPages).fill(0).map((x,i)=>i+1);
+        this.currentPage = 1;
       });
   }
 
   changePage(page:number): void {
-    console.log('change page');
     this.currentPage = page;
     this.postingService.getPostings(page)
       .subscribe(posts => {
@@ -38,10 +39,13 @@ export class PostingsComponent implements OnInit {
       });
   }
 
+  // Called through eventemitter from search component
   searchPostings(event): void {
     this.postingService.getPostings(1, event.position, event.location)
       .subscribe(posts => {
         this.postings = posts.data;
+        this.totalPages = posts.totalPages;
+        this.totalPagesArr = Array(this.totalPages).fill(0).map((x,i)=>i+1);
       });
   }
 }
